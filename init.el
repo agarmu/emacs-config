f2ee4608d7461ccc68a2e3593d1c9014b3133cc7
Mukul Agarwal <agarmukul23@gmail.com>
2024-08-08 13:02:34 -0500

    remove indent-guides

--- init.el
+++ init.el
@@ -35,7 +35,7 @@
        doom-quit           ; DOOM quit-message prompts when you quit Emacs
        ;;(emoji +unicode)  ; 🙂
        hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       indent-guides       ; highlighted indent columns
+       ;;indent-guides     ; highlighted indent columns
        ligatures           ; ligatures and symbols to make your code pretty again
        modeline            ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
